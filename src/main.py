d9c48b62edb1aae2a39c1e3a908d6b0d166f0a4b
ALIBEK <alibeksadirbaev2009@gmail.com>
2026-07-10 18:22:27 +0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,46 +3,20 @@ import logging
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
-from .config import TOKEN
+from .config import settings
 
-bot = Bot(TOKEN)
+bot = Bot(settings.COW)
 dp = Dispatcher()
-
+# start comandasi
 @dp.message(CommandStart())
 async def start(message: Message):
     u = message.from_user
-    await message.answer(f"Hello, {u.first_name}")
+    await message.answer(f"Sa'lem, {u.first_name}")
 
-
-@dp.message(Command("help"))
-async def help(message: Message):
-    await message.answer(
-        "/start - Botti iske tu'siriw\n"
-        "/help - Ja'rdem\n" 
-        "/contact - contact almasiw"
-    )
-
-@dp.message(Command("contact"))
-async def get_contact(message: Message):
-    await message.answer(f"Nomer:")
-    await message.answer(f"At:")
-
-
-@dp.message(F.text.lower() == "bye")
-async def bye(message: Message):
-    await message.answer(f"Saw aman bol")
-
-@dp.message(F.text)
-async def text(message: Message):
-    text = message.text
-    await message.answer("Text qabil qilinbaydi!")
-
-
-
-
+# bul bot iske qosilganda info aliw yamasa isletillgen de
 async def main():
     logging.basicConfig(level=logging.INFO)
     await dp.start_polling(bot)
 
-if __name__ == "__main__": 
+if __name__ == "__main__":  
     asyncio.run(main=main())
